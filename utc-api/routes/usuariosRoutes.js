4e6cde35fd1ca559034493715ec8d231e282235f
Enrique <e.resendiz.r688@edu.utc.mx>
2026-06-22 09:11:47 -0600

    correcion de permisos practicantes

--- utc-api/routes/usuariosRoutes.js
+++ utc-api/routes/usuariosRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const usuariosController = require('../controllers/usuariosController');
 const { requireAuth, requireRole, requireSameArea } = require('../middleware/authMiddleware');
 
-router.get('/usuarios', requireAuth, requireRole(['admin', 'master']), usuariosController.getAll);
+router.get('/usuarios', requireAuth, requireRole(['admin', 'master', 'practicante']), usuariosController.getAll);
 router.get('/usuarios/:id', requireAuth, usuariosController.getById);
 router.patch('/usuarios/:id', requireAuth, usuariosController.updateProfile);
 router.put('/usuarios/:id', requireAuth, requireRole(['admin', 'master']), requireSameArea, usuariosController.updateStatus);
